c091ba36249fb7e866b7918af7c5e2f892ef614a
Babar Asghar <babarali@gmail.com>
2018-04-03 14:00:58 +0500

    Adds acceptable origins for the pdf.

--- pdf.js-viewer/web/viewer.js
+++ pdf.js-viewer/web/viewer.js
@@ -1886,28 +1886,40 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
-  validateFileURL = function validateFileURL(file) {
-    if (file === undefined) {
-      return;
-    }
-    try {
-      var viewerOrigin = new URL(window.location.href).origin || 'null';
-      if (HOSTED_VIEWER_ORIGINS.indexOf(viewerOrigin) >= 0) {
-        return;
-      }
-      var fileOrigin = new URL(file, window.location.href).origin;
-      if (fileOrigin !== viewerOrigin) {
-        throw new Error('file origin does not match viewer\'s');
-      }
-    } catch (ex) {
-      var message = ex && ex.message;
-      PDFViewerApplication.l10n.get('loading_error', null, 'An error occurred while loading the PDF.').then(function (loadingErrorMessage) {
-        PDFViewerApplication.error(loadingErrorMessage, { message: message });
-      });
-      throw ex;
-    }
-  };
+    var HOSTED_VIEWER_ORIGINS = [
+        'null',
+        'http://api.conta.test',
+        'http://app.conta.test',
+        'https://stage.conta.no',
+        'https://conta.no',
+        'https://app.conta.no'
+    ];
+
+    validateFileURL = function validateFileURL(file) {
+        if (file === undefined) {
+            return;
+        }
+        try {
+            var viewerOrigin = new URL(window.location.href).origin,
+                fileOrigin = new URL(file, window.location.href).origin;
+
+            if (HOSTED_VIEWER_ORIGINS.indexOf(fileOrigin || 'null') >= 0) {
+                return;
+            }
+
+            if (fileOrigin !== viewerOrigin) {
+                throw new Error('file origin does not match viewer\'s');
+            }
+        } catch (ex) {
+            var message = ex && ex.message;
+
+            PDFViewerApplication.l10n.get('loading_error', null, 'An error occurred while loading the PDF.').then(function (loadingErrorMessage) {
+                PDFViewerApplication.error(loadingErrorMessage, { message: message });
+            });
+
+            throw ex;
+        }
+    };
 }
 function loadAndEnablePDFBug(enabledTabs) {
   return new Promise(function (resolve, reject) {
